987638e8bb587fdec835c120d49d8a380fe9984b
Renaud Racinet <renaud.racinet@estiam.com>
2019-09-12 14:49:54 +0200

    feat(NavigationComponent): Route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NavigationComponent} from './components/navigation/navigation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavigationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
