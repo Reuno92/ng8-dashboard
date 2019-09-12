64408897d5cad9bf1b287ce9efe1d48ac1505902
Renaud Racinet <renaud.racinet@estiam.com>
2019-09-12 14:58:58 +0200

    Add landing page and his routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import {LandingComponent} from './components/landing/landing.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: NavigationComponent
+    component: NavigationComponent,
+    children: [
+      {
+        path: '',
+        component: LandingComponent
+      }
+    ]
   }
 ];
 
